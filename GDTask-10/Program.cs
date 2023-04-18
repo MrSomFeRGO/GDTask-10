41c27c10214e79cacd6310f26393714a630ff8a7
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 11:14:00 +0300

    Initial commit

---
+++ GDTask-10/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine(cheto_delaet("Привет, сын хорошей женщины!"));
+
+static string cheto_delaet(string input)
+{
+    string output = "";
+    string[] words = input.Split(' ');
+    foreach (var el in words)
+    {
+        string new_world = "";
+        if (el.Contains(',') | el.Contains('.') | el.Contains('!') | el.Contains('?'))
+        {
+            new_world = el.Substring(1, el.Length - 2);
+            new_world += el.Substring(0, 1);
+            new_world += "ай";
+            new_world += el.Substring(el.Length - 1);
+        }
+        else
+        {
+            new_world = el.Substring(1);
+            new_world += el.Substring(0, 1);
+            new_world += "ай";
+        }
+
+        output += new_world + " ";
+    }
+
+    return output;
+}
